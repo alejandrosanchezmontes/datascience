f86dfca85d880bef0944daa7968062f74ca273fb
Alejandro sanchezm <alexsanchezm@hotmail.com>
2020-10-23 00:08:10 +0000

    clase de jueves

--- hola_mundo.R
+++ hola_mundo.R
@@ -1,1 +1,13 @@
-print("Hola mundo")
+read.csv("~/datascience/data.csv")
+install.packages(c("httr"),"jsonlite")
+library(httr)
+library(jsonlite)
+result = GET("http://api.open-notify.org/astros.json")
+result$content
+rawToChar(result$content)
+data = fromJSON(rawToChar(result$content))
+data
+result2 = GET("https://swapi.dev/api/people/1")
+data2 = fromJSON(rawToChar(result2$content))
+result2$content
+                 
